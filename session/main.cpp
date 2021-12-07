d72b57c0fafe4298557a101c74c4aefcf011ae36
POZ <antoivans@gmail.com>
2021-12-08 00:57:13 +0300

    Добавлены тестовые данные

--- session/main.cpp
+++ session/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #8. Session results\n";
+    cout << "Variant #0. Session results\n";
     cout << "Author: Dmitriy Panin\n";
+    cout << "Group: 14\n";
     return 0;
 }
